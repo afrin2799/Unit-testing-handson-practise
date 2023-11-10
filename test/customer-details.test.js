90f1c85af1fa400afa1b70f1e5b4a197c852d737
AkuthotaAnisa <anisaakuthota@gmail.com>
2023-11-10 10:58:12 +0530

    Signed-off-by: AkuthotaAnisa <anisaakuthota@gmail.com>

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -5,31 +5,24 @@ import '../src/Customer/Customer-details.js';
 import Sinon from 'sinon';
 
 
-describe('customer details', () => {
+describe('customer details', async() => {
   // Write test cases inside this block
-
+  const el=await fixture(html`<customer-details></customer-details>`);
 
   it('checks for handlers', async()=>{
-    const el=await fixture(html`<customer-details></customer-details>`);
+    
     await el.shadowRoot.querySelector('#nextbtn').click();
   });
+  it('should display the form fields',()=>{
+		const firstName=el.shadowRoot.querySelector('#first_name');
+		const lastName=el.shadowRoot.querySelector('#last_name');
+		const email=el.shadowRoot.querySelector('#email');
+		const mobile=el.shadowRoot.querySelector('#mobile_number');
+		expect(firstName).to.exist;
+		expect(lastName).to.exist;
+		expect(email).to.exist;
+		expect(mobile).to.exist;
+	});
   
-  
-/*
-  it('should display an error message for first_name input with length less than 3', async () => {
-    const element = await fixture(html`<customer-details></customer-details>`);
-  
-    const input = element.shadowRoot.querySelector('#first_name');
-    input.value = 'ab'; // Set a value less than 3 characters
-
-    const form = element.shadowRoot.querySelector('lion-form');
-    await form.submit();
-    
-    const errorMessage = input.errorStateMessage;
-    expect(errorMessage).not.equal('First name is a required field');
-//Assertion error
-   
-  });
-  */
 
 });
