f755f43576097cadfe991b4ba1dd2e026bbc9400
AkuthotaAnisa <anisaakuthota@gmail.com>
2023-11-10 19:13:54 +0530

    Signed-off-by: AkuthotaAnisa <anisaakuthota@gmail.com>

--- test/headers.test.js
+++ test/headers.test.js
@@ -4,7 +4,7 @@ import '../src/header/Header.js';
 
 describe('loan-header', () => {
   // Write test cases inside this block
-  it('checks for handlers', async()=>{
+  /*it('checks for handlers', async()=>{
     const el=await fixture(html`<loan-header></loan-header>`);
     await el.shadowRoot.querySelector('#nl-NL').click();
   });
@@ -12,11 +12,17 @@ describe('loan-header', () => {
     const el=await fixture(html`<loan-header></loan-header>`);
     await el.shadowRoot.querySelector('#en-GB').click();
   });
+  */
   it('renders correctly', async() => {
     const el=await fixture(html`<loan-header></loan-header>`);
     const enButton = el.shadowRoot.getElementById('en-GB');
     const nlButton = el.shadowRoot.getElementById('nl-NL');
     expect(enButton).to.be.ok;
     expect(nlButton).to.be.ok;
+  });
+  it('passes accessibility test for NL', async () => {
+    const el = await fixture(html` <loan-header></loan-header> `);
+    const myval1= await el.shadowRoot.querySelectorAll('button');
+    await expect(myval1).to.be.accessible();
   });
 });
